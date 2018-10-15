d97c09b16767904789e832443558dff15b381d77
Al <beattie.edwin@gmail.com>
2018-10-15 22:01:24 +0100

    Change Style Sheet

--- Likey/App_Start/BundleConfig.cs
+++ Likey/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Likey
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/superhero.css",
                       "~/Content/site.css"));
         }
     }
