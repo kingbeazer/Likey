c2f07bbf9ea4df2877086bce560fe3eae85c35f5
Al <beattie.edwin@gmail.com>
2018-10-16 23:03:44 +0100

    Added Likes

---
+++ Likey/Controllers/LikesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Likey.Controllers
+{
+    public class LikesController : Controller
+    {
+        // GET: Likes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Likey/Models/Movie.cs
+++ Likey/Models/Movie.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Likey.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
 
